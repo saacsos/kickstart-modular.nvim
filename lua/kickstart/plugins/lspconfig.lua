3a3a1ba91aced04d38bcc3b4a138ee71ede0506f
Thanawat Potidet <thanawatpotidech@gmail.com>
2024-08-06 16:51:53 +0700

    user ENTER to confirm selection cmp.lua add more lsplanguage lspconfig.lua add customplugin and use more plugin such as neotree twopair plugins.lua

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -166,9 +166,12 @@ return {
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
-        -- clangd = {},
-        -- gopls = {},
-        -- pyright = {},
+        clangd = {},
+        gopls = {},
+        pyright = {
+          capabilities = capabilities,
+        },
+        jdtls = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
@@ -176,7 +179,7 @@ return {
         --    https://github.com/pmizio/typescript-tools.nvim
         --
         -- But for many setups, the LSP (`tsserver`) will work just fine
-        -- tsserver = {},
+        tsserver = {},
         --
 
         lua_ls = {
