f1a0d28666bd4589f0ed1f5bd608a8650989f9b1
Thanawat Potidet <thanawatpotidech@gmail.com>
2024-08-06 20:45:54 +0700

    add vim.g.have_nerd_font = true to tell neovim that we have nerd font in machine

--- lua/options.lua
+++ lua/options.lua
@@ -9,6 +9,8 @@ vim.opt.number = true
 --  Experiment for yourself to see if you like it!
 vim.opt.relativenumber = true
 
+-- Set to true if you have a Nerd Font installed and selected in the terminal
+vim.g.have_nerd_font = true
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.opt.mouse = 'a'
 
